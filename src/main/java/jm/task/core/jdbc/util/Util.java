c1f4d8d1e9bd128d5e2a994e0b2476000c3a46be
Andry Izyurov <izyurof@gmail.com>
2024-01-22 11:59:53 +0300

    Added logic for connecting to the database in the class Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,47 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverAction;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final Properties PROPERTIES = new Properties();
+    private static final String URL_KEY = "url";
+    private static final String USERNAME_KEY = "username";
+    private static final String PASSWORD_KEY = "username";
+    
+    static {
+        loadProperties();
+    }
+
+    private Util() {
+
+    }
+
+    private static void loadProperties() {
+        try (InputStream inputStream = Util.class.getClassLoader().getResourceAsStream("application.properties")) {
+            PROPERTIES.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getValue(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+
+    public static Connection openConnection() {
+        try {
+            return DriverManager.getConnection(PROPERTIES.getProperty(URL_KEY),
+                    PROPERTIES.getProperty(USERNAME_KEY),
+                    PROPERTIES.getProperty(PASSWORD_KEY));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
