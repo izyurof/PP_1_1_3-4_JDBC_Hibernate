b92b30ca4ff7bb98f373d7abd0206bfa279452b2
AndryIzyurov <izyurof@gmail.com>
2024-01-28 12:48:20 +0300

    added implementation for hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,87 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private SessionFactory sessionFactory;
     public UserDaoHibernateImpl() {
+    }
 
+    public UserDaoHibernateImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
 
     @Override
     public void createUsersTable() {
+        String createTable = """
+                create table if not exists users (
+                id serial primary key,
+                name text,
+                last_name text,
+                age int
+                );
+                """;
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.createSQLQuery(createTable).executeUpdate();
+        session.getTransaction().commit();
 
     }
 
     @Override
     public void dropUsersTable() {
+        String dropTable = """
+                drop table if exists users;
+                """;
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.createSQLQuery(dropTable).executeUpdate();
+        session.getTransaction().commit();
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.save(User.builder()
+                .name(name)
+                .lastName(lastName)
+                .age(age)
+                .build());
+        session.getTransaction().commit();
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        User user = session.get(User.class, id);
+        session.delete(user);
+        session.getTransaction().commit();
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        String hql = "select u from User u";
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        List<User> userList = session.createQuery(hql, User.class).list();
+        session.getTransaction().commit();
+        return userList;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        String hql = "delete from User";
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.createQuery(hql).executeUpdate();
+        session.getTransaction().commit();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,35 +1,38 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDao userDao = new UserDaoJDBCImpl();
+    private UserDao userDaoJDBS = new UserDaoJDBCImpl();
+    private UserDao userDaoHibernate = new UserDaoHibernateImpl(Util.getSessionFactory());
 
     public void createUsersTable() {
-        userDao.createUsersTable();
+        userDaoHibernate.createUsersTable();
     }
 
     public void dropUsersTable() {
-        userDao.dropUsersTable();
+        userDaoHibernate.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        userDao.saveUser(name, lastName, age);
+        userDaoHibernate.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        userDao.removeUserById(id);
+        userDaoHibernate.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return userDao.getAllUsers();
+        return userDaoHibernate.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        userDao.cleanUsersTable();
+        userDaoHibernate.cleanUsersTable();
     }
 }
